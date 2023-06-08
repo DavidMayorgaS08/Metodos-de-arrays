84f29af6aa4d6566d0b0a3177e0e9cd7fc76b4b9
DavidMayorgaS08 <david08102@hotmail.com>
2023-06-07 20:32:44 -0500

    Fin Includes

---
+++ Sort/ejer1.js
@@ -0,0 +1,5 @@
+const numeros = [1, 4, 5, 6, 2, 3, 7, 8, 9, 10, 12, 11, 15, 14, 13];
+
+const numerosOrdenados = numeros.sort((a, b) => a - b);
+
+console.log(numerosOrdenados);
---
+++ Sort/ejer2.js
@@ -0,0 +1,5 @@
+const Pardesordenados = [4, 12, 8, 130, 44, 34];
+
+const ParOrdenados = Pardesordenados.sort((a, b) => a - b);
+
+console.log(ParOrdenados);
