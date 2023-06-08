8bdefe3e42803fd1369a542c884a7e9299bf5d4b
DavidMayorgaS08 <david08102@hotmail.com>
2023-06-07 20:42:22 -0500

    Concat

---
+++ Concat/ejer1.js
@@ -0,0 +1,6 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+
+const juntos = numeros.concat(letras);
+
+console.log(juntos);
---
+++ Concat/ejer2.js
@@ -0,0 +1,6 @@
+const mitadAbecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+const otraMitadAbecedario = ["j", "k", "l", "m", "n", "o", "p", "q", "r"];
+
+const abecedario = mitadAbecedario.concat(otraMitadAbecedario);
+
+console.log(abecedario);
