6a6f36e4ae14f68e08e490ee50b926c83e3183bd
DavidMayorgaS08 <david08102@hotmail.com>
2023-06-07 20:23:49 -0500

    Some

---
+++ Some/ejer1.js
@@ -0,0 +1,6 @@
+const numeros = [1, 2, 6, 3, 4, 5];
+
+const par = numeros.some(element => element % 2 === 0);
+
+console.log(par);
+
---
+++ Some/ejer2.js
@@ -0,0 +1,7 @@
+const edades = [25, 18, 10, 15, 21, 35];
+
+const mayor = edades.some(element => element === 19);
+
+console.log(mayor);
+
+
